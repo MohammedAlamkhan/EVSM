1c91167909d7a7c38d90be96ee3fed450f7aa965
Mohammed Alam <mohammedalamkhan6@gmail.com>
2022-05-23 15:56:48 +0530

    account api integration

--- src/app/main/admin/accounts/accounts.component.ts
+++ src/app/main/admin/accounts/accounts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AccountsService } from './accounts.service';
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
@@ -8,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class AccountComponent implements OnInit {
   public contentHeader: object
   accounts: any;
+  loading: boolean = false;
 
 
-  constructor() { }
+  constructor(private accountsService: AccountsService) { }
   ngOnInit(): void {
     this.contentHeader = {
       headerTitle: 'Accounts',
@@ -26,72 +27,17 @@ export class AccountComponent implements OnInit {
         ]
       }
     };
-
-    this.accounts={
-      "content": [
-          {
-              "accountTypeName": "Self",
-              "shippingStateId": 12,
-              "modifiedDate": "2022-05-20T11:02:55.000+00:00",
-              "billingStateName": "Haryana",
-              "shippingStreet": null,
-              "billingAddress": "Plot No. 38, Institutional Area, Sector 32",
-              "createdDate": "2022-05-20T11:02:55.000+00:00",
-              "description": null,
-              "shippingAddress": "Plot No. 38, Institutional Area, Sector 32",
-              "shippingPincode": "122001",
-              "contactPersonPhone": null,
-              "billingStateId": 12,
-              "createByName": "System Admin",
-              "sapCode": "Exicom",
-              "createdByPhone": "9999999999",
-              "isSynced": false,
-              "id": 1,
-              "billingCity": "Gurgaon",
-              "billingPincode": "122001",
-              "modifiedByName": "System Admin",
-              "shippingCity": "Gurgaon",
-              "ownerName": "System Admin",
-              "shippingStateName": "Haryana",
-              "accountTypeId": 2,
-              "contactPersonName": null,
-              "billingStreet": null,
-              "billingCountry": "India",
-              "name": "Exicom",
-              "shippingCountry": "India",
-              "modifiedbyPhone": "9999999999",
-              "ownerPhone": "9999999999",
-              "contactPersonEmail": null,
-              "customer": null
-          }
-      ],
-      "pageable": {
-          "sort": {
-              "empty": true,
-              "sorted": false,
-              "unsorted": true
-          },
-          "offset": 0,
-          "pageNumber": 0,
-          "pageSize": 20,
-          "unpaged": false,
-          "paged": true
-      },
-      "totalElements": 1,
-      "totalPages": 1,
-      "last": true,
-      "size": 20,
-      "number": 0,
-      "sort": {
-          "empty": true,
-          "sorted": false,
-          "unsorted": true
-      },
-      "numberOfElements": 1,
-      "first": true,
-      "empty": false
+    this.loadData();
   }
-   
+
+  loadData() {
+    this.loading = true;
+    this.accounts =   this.accountsService.getAccountsInformation({}).subscribe(
+      (data) => {
+        this.accounts = data.content;
+        this.loading = false;
+      }
+    )
   }
 
 }
