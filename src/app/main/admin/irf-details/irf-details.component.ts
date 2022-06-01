295d347b1e17afc87790b2558d562ef8cce337f3
Mohammed Alam <mohammedalamkhan6@gmail.com>
2022-06-01 23:32:38 +0530

    irf worklist details

--- src/app/main/admin/irf-details/irf-details.component.ts
+++ src/app/main/admin/irf-details/irf-details.component.ts
@@ -8,6 +8,7 @@ import {IrfService} from './../irf/irf.service'
 export class IrfDetailsComponent implements OnInit {
 public contentHeader : object
   irf: any;
+  irfworklistMap: {};
   constructor(private irfService: IrfService) { }
 
   ngOnInit(): void {
@@ -37,6 +38,31 @@ public contentHeader : object
     };
 
     this.irf = this.irfService.selectedIrf;
+    
+    this.irfworklistMap={
+      "1":"Civil Plinth/Stand",
+      "2":"Commissioning",
+      "3":"Earthing Pit Material",
+      "4":"Electric Vehicle for Testing",
+      "5":"Fork Lift/Hydra Crane for Charger Mounting",
+      "6":"Installation",
+      "7":"Installation-DB Box",
+      "8":"Installation-Earthing Pit",
+      "9":"Installation-Energy Meter",
+      "10":"Installation-MCB/MCCB",
+      "11":"Installation-Meter Box",
+      "12":"Laying/Fixing-Earth Cable",
+      "13":"Laying/Fixing-Input Cable",
+      "14":"Site Survey",
+      "15":"Supply of DB Box",
+      "16":"Supply of Earth Cable",
+      "17":"Supply of Energy Meter",
+      "18":"Supply of Input Cable",
+      "19":"Supply of IP/MCB/MCCB",
+      "20":"Supply of Meter Box"
     }
+       
+  
+  }
 
 }
